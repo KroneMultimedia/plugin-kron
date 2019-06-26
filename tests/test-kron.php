f186ea4991ca0cf18fccc557ac736d638666f627
Helmut Januschka <helmut@januschka.com>
2019-06-26 08:21:00 +0200

    silent tests

--- tests/test-kron.php
+++ tests/test-kron.php
@@ -12,22 +12,6 @@ class TestKron extends \WP_UnitTestCase
     */
     public function dummy()
     {
-
-      
-        wp_schedule_single_event(time()+20, 'single_shot_event', []);
-        exit;
-        return;
-        $time = strtotime('00:00:00');
-        sleep(5);
-      
-
-        return;
-
-        wp_schedule_event($time, 'daily', 'krn_delete_ngen_articles_preview_images1');
-        wp_schedule_single_event($time, 'single_shot_event', []);
-        wp_schedule_single_event($time, 'single_shot_event', []);
-        if (! wp_next_scheduled('krn_delete_ngen_articles_preview_images')) {
-            wp_schedule_event($time, 'daily', 'krn_delete_ngen_articles_preview_images');
-        }
+      $this->assertEquals(1,1);
     }
 }
